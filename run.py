570fec807c63f7e01dbdfb0120ce97ced74eb7d3
Natalitta <nataliialebedieva@gmail.com>
2023-03-26 13:19:53 +0000

    Add get sales num function to get the amount of sold items and update it to the worksheet

--- run.py
+++ run.py
@@ -20,12 +20,12 @@ def get_bought_num():
     for each colour of orchids bought to the stock.
     A while loop will ask for the input all over again
     until the data entered is valid. 
-    There mush be exactly 4 numbers separated by commas.
+    There must be exactly 4 numbers separated by commas.
     """
     while True:
         print("." * 50)
         print("Please enter the number of orchids you ordered for sale.")
-        print("Type numbers separated by comma in the following order:\n")
+        print("Type numbers separated by commas in the following order:\n")
         print(" white\n pink\n yellow\n purple\n")
         print("Example: 25, 30, 50, 45")
         print("." * 50)
@@ -71,21 +71,59 @@ def update_bought_worksheet(bought_data):
     print("Worksheet updated successfully\n")
 
 
-def calculate_money_spent(bought_money_data):
+def get_sales_num():
     """
+    Requests a number from the user for each colour of orchids sold.
+    A while loop will ask for the input all over again
+    until the data entered is valid. 
+    There must be exactly 4 numbers separated by commas.
+    """
+    while True:
+        print("." * 50)
+        print("Please enter the number of orchids you sold.")
+        print("Type numbers separated by commas in the following order:\n")
+        print(" white\n pink\n yellow\n purple\n")
+        print("Example: 25, 30, 50, 45")
+        print("." * 50)
+
+        sales_num = input("Enter numbers here: ")
+        print(f"Your entered {sales_num}\n")
+
+        sales_num_data = sales_num.split(",")
+        
+        if valid_data(sales_num_data):
+            print("Data is valid\n")
+            break
+    return sales_num_data
+
+
+def update_sales_worksheet(sales_data):
+    """
+    Updates the bought worksheet with numbers of items bought to stock.
+    Adds a new row of data to the list.
+    """
+    print("Updating the worksheet with new information...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(sales_data)
+    print("Worksheet updated successfully\n")
+
+
+def calculate_surplus(sales_row):
+
+
+"""
+def calculate_money_spent(bought_money_data):
+    
     Calculates money spent for each type to calculate profit later.
     Takes a number of ordered orchids and multiplies by trade price.
     Trade price is 7 euros.
-    """
+    
     print("Calculating costs...\n")
     bought_money = bought_data * 7
     bought_money_worksheet = SHEET.worksheet("bought-money")
     bought_money_worksheet.append_row(bought_money_data)
     print("Worksheet updated successfully\n")
-
-     
-   
-
+"""
 
 def main():
     """
@@ -94,7 +132,10 @@ def main():
     bought_data = get_bought_num()
     bought_data_int = [int(num) for num in bought_data]
     update_bought_worksheet(bought_data_int)
-    calculate_money_spent(bought_data_int)
+    sales_data = get_sales_num()
+    sales_data_int = [int(num) for num in sales_data]
+    update_sales_worksheet(sales_data_int)
+    #calculate_money_spent(bought_data_int)
 
 
 print("." * 50)
